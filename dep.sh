1512f5b7a21105604e01a1f3dc794443e7604646
clouddg15 <talekarshraddha00@gmail.com>
2025-10-05 16:32:53 +0530

    my commit 4 files

--- dep.sh
+++ dep.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 sudo apt-get install apache2 -y
-rm /var/www/html/home.html /var/www/html/about.html /var/www/html/contact.html /var/www/html/loginform.html
+rm /var/www/html/home.html 
+rm /var/www/html/about.html 
+rm /var/www/html/contact.html 
+rm /var/www/html/loginform.html
 
