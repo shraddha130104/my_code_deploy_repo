edc038543dc1f79134bb9fde9c769e05c9dcbf44
shraddha130104 <talekarshraddha00@gmail.com>
2025-12-11 21:40:29 +0530

    Fix rm command to use -f flag for file removal

--- dep.sh
+++ dep.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo apt-get install apache2 -y
-rm /var/www/html/home.html 
- /var/www/html/about.html 
- /var/www/html/contact.html 
- /var/www/html/loginform.html
-
+rm -f /var/www/html/home.html
+rm -f /var/www/html/about.html
+rm -f /var/www/html/contact.html
+rm -f /var/www/html/loginform.html
